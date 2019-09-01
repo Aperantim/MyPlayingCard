53dd569e1b78a40f5b0331a96ef1d3bcbd84c0c7
Vitaliy Nesterov <liyuop@mail.ru>
2019-09-01 17:36:14 +0300

    Initial Commit

---
+++ MyPlayingCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlayingCard
+//
+//  Created by Виталий on 01/09/2019.
+//  Copyright © 2019 Aperantim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
